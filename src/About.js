891e1c70688cfa00797a2284f2c025ad388762c3
RohitJaiswar123 <rohit.s.jaiswar@gmail.com>
2023-08-02 13:51:12 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+    <main className="About">
+        <h1>About</h1>
+        <p style={{marginTop: "1rem"}}>This is React Blog App Project</p>
+    </main>
+    )
+  }
+  
+  export default About
